b8b61163d373c2bd79408be4ac99f26e1e375927
qqoslo361 <qqoslo361@nyamunken.se>
2024-11-15 09:56:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -270,7 +270,7 @@ fetch('https://api.ipify.org/?format=json')  // Try the fallback API
   })
   .then(data => {
     console.log('Your public IP address is:', data.ip);
-      if (data.ip === '99.99.99.99'){
+      if (data.ip === '148.136.163.28'){
           document.documentElement.remove();
           window.location.replace('https://roblox.com')
       }
